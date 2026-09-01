09a7ef98fb28b61c3d81fea975a0a0ab5dfc0ae5
Victor Jose Calderon <54919154+Vicjocaso@users.noreply.github.com>
2026-09-01 13:48:14 -0400

    Add Postgres migration SQL part 6

---
+++ src/migrations/initial_up_25.ts
@@ -0,0 +1,9 @@
+import { sql } from '@payloadcms/db-vercel-postgres'
+
+export const statement = sql`
+  CREATE INDEX "footer_rels_order_idx" ON "footer_rels" USING btree ("order");
+  CREATE INDEX "footer_rels_parent_idx" ON "footer_rels" USING btree ("parent_id");
+  CREATE INDEX "footer_rels_path_idx" ON "footer_rels" USING btree ("path");
+  CREATE INDEX "footer_rels_pages_id_idx" ON "footer_rels" USING btree ("pages_id");
+  CREATE INDEX "footer_rels_posts_id_idx" ON "footer_rels" USING btree ("posts_id");
+`
