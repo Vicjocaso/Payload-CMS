bcc270e76a6344cbadb196c59de01c469c81a385
Victor Jose Calderon <54919154+Vicjocaso@users.noreply.github.com>
2026-08-31 21:44:25 -0400

    Add AdminBar, BeforeDashboard, BeforeLogin, and Card components

---
+++ src/components/BeforeLogin/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BeforeLogin: React.FC = () => {
+  return (
+    <div>
+      <p>
+        <b>Welcome to your dashboard!</b>
+        {' This is where you log in to edit Atelier Wellness pages, insights, and media.'}
+      </p>
+    </div>
+  )
+}
+
+export default BeforeLogin
