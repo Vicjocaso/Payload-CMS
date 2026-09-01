8eeba504e659bebc1815e36e12ba782c2aec76b7
Victor Jose Calderon <54919154+Vicjocaso@users.noreply.github.com>
2026-09-01 14:07:11 -0400

    Add SQL migration parts outside Payload's migration folder

---
+++ src/migration-parts/initial_up_07.ts
@@ -0,0 +1,115 @@
+import { sql } from '@payloadcms/db-vercel-postgres'
+
+export const statement = sql`
+  CREATE TABLE "forms_blocks_number" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"_path" text NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"label" varchar,
+  	"width" numeric,
+  	"default_value" numeric,
+  	"required" boolean,
+  	"block_name" varchar
+  );
+  
+  CREATE TABLE "forms_blocks_select_options" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" varchar NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"label" varchar NOT NULL,
+  	"value" varchar NOT NULL
+  );
+  
+  CREATE TABLE "forms_blocks_select" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"_path" text NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"label" varchar,
+  	"width" numeric,
+  	"default_value" varchar,
+  	"placeholder" varchar,
+  	"required" boolean,
+  	"block_name" varchar
+  );
+  
+  CREATE TABLE "forms_blocks_state" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"_path" text NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"label" varchar,
+  	"width" numeric,
+  	"required" boolean,
+  	"block_name" varchar
+  );
+  
+  CREATE TABLE "forms_blocks_text" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"_path" text NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"label" varchar,
+  	"width" numeric,
+  	"default_value" varchar,
+  	"required" boolean,
+  	"block_name" varchar
+  );
+  
+  CREATE TABLE "forms_blocks_textarea" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"_path" text NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"name" varchar NOT NULL,
+  	"label" varchar,
+  	"width" numeric,
+  	"default_value" varchar,
+  	"required" boolean,
+  	"block_name" varchar
+  );
+  
+  CREATE TABLE "forms_emails" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"email_to" varchar,
+  	"cc" varchar,
+  	"bcc" varchar,
+  	"reply_to" varchar,
+  	"email_from" varchar,
+  	"subject" varchar DEFAULT 'You''ve received a new message.' NOT NULL,
+  	"message" jsonb
+  );
+  
+  CREATE TABLE "forms" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"title" varchar NOT NULL,
+  	"submit_button_label" varchar,
+  	"confirmation_type" "enum_forms_confirmation_type" DEFAULT 'message',
+  	"confirmation_message" jsonb,
+  	"redirect_url" varchar,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+  
+  CREATE TABLE "form_submissions_submission_data" (
+  	"_order" integer NOT NULL,
+  	"_parent_id" integer NOT NULL,
+  	"id" varchar PRIMARY KEY NOT NULL,
+  	"field" varchar NOT NULL,
+  	"value" varchar NOT NULL
+  );
+  
+  CREATE TABLE "form_submissions" (
+  	"id" serial PRIMARY KEY NOT NULL,
+  	"form_id" integer NOT NULL,
+  	"updated_at" timestamp(3) with time zone DEFAULT now() NOT NULL,
+  	"created_at" timestamp(3) with time zone DEFAULT now() NOT NULL
+  );
+`
