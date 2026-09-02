f72ebee8c56152d71e61d8f7c33b0fc1655c78fa
Victor Jose Calderon <54919154+Vicjocaso@users.noreply.github.com>
2026-09-02 10:18:37 -0400

    Add Site Settings global for editable site name and logo

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260901_171920_initial from './20260901_171920_initial';
 import * as migration_20260902_001541_page_templates from './20260902_001541_page_templates';
+import * as migration_20260902_141321_site_settings from './20260902_141321_site_settings';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260902_001541_page_templates.up,
     down: migration_20260902_001541_page_templates.down,
-    name: '20260902_001541_page_templates'
+    name: '20260902_001541_page_templates',
+  },
+  {
+    up: migration_20260902_141321_site_settings.up,
+    down: migration_20260902_141321_site_settings.down,
+    name: '20260902_141321_site_settings'
   },
 ];
