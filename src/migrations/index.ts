1c6a32d87606c4f24d31bf8a5fa805d2e485664a
Victor Jose Calderon <54919154+Vicjocaso@users.noreply.github.com>
2026-09-01 20:19:17 -0400

    Add Page Templates admin section for reusable page layouts

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260901_171920_initial from './20260901_171920_initial';
+import * as migration_20260902_001541_page_templates from './20260902_001541_page_templates';
 
 export const migrations = [
   {
     up: migration_20260901_171920_initial.up,
     down: migration_20260901_171920_initial.down,
-    name: '20260901_171920_initial'
+    name: '20260901_171920_initial',
+  },
+  {
+    up: migration_20260902_001541_page_templates.up,
+    down: migration_20260902_001541_page_templates.down,
+    name: '20260902_001541_page_templates'
   },
 ];
